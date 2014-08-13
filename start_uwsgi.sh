f5aba0c2a28ccb4b11256fa17c07cc39cda87090
sprin <sprin>
2014-08-13 16:20:21 -0700

    Remove uwsgi config from source

--- start_uwsgi.sh
+++ start_uwsgi.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 exec /usr/local/bin/uwsgi \
-    --http=:8080 \
+    --http-socket=:8080 \
     --wsgi-file=./src/app.py \
-    --callable=app \
-    --gevent=$GEVENT_WORKERS \
-    --master=$UWSGI_MASTER \
-    --die-on-term
-
+    --callable=app
